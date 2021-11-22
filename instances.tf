6351ce77e39cade69ec8aba436c19d143dd7dc86
Chyngyz Toichubekov <t.chyngyz@yandex.ru>
2021-11-22 11:25:44 +0600

    Third commit: EC2 instances

--- instances.tf
+++ instances.tf
@@ -7,7 +7,7 @@ data "aws_ssm_parameter" "ApacheLabAmi" {
 #Create key-pair for logging into EC2 in us-east-1
 resource "aws_key_pair" "master-key" {
   provider   = aws.region-lab
-  key_name   = "jenkins"
+  key_name   = "apache"
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
@@ -17,9 +17,9 @@ resource "aws_instance" "ApacheLabInt" {
   ami                         = data.aws_ssm_parameter.ApacheLabAmi.value
   instance_type               = var.instance-type
   key_name                    = aws_key_pair.master-key.key_name
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.jenkins-sg.id]
+  vpc_security_group_ids      = [aws_security_group.app-sg.id]
   subnet_id                   = aws_subnet.subnet_app.id
+  #ecs_associate_public_ip_address = "false"
 #  provisioner "local-exec" {
 #    command = <<EOF
 #aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-master} --instance-ids ${self.id} \
@@ -27,7 +27,28 @@ resource "aws_instance" "ApacheLabInt" {
 #EOF
 #  }
   tags = {
-    Name = "jenkins_master_tf"
+    Name = "apache_tf"
   }
-  depends_on = [aws_main_route_table_association.set-master-default-rt-assoc]
+#  depends_on = [aws_main_route_table_association.set-master-default-rt-assoc]
+}
+
+#Create and bootstrap EC2 in us-east-1
+resource "aws_instance" "BastionLabInt" {
+  provider                    = aws.region-lab
+  ami                         = data.aws_ssm_parameter.ApacheLabAmi.value
+  instance_type               = var.instance-type
+  key_name                    = aws_key_pair.master-key.key_name
+  vpc_security_group_ids      = [aws_security_group.bastion-sg.id]
+  subnet_id                   = aws_subnet.subnet_bastion.id
+  #ecs_associate_public_ip_address = "true"
+#  provisioner "local-exec" {
+#    command = <<EOF
+#aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-master} --instance-ids ${self.id} \
+#&& ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/install_jenkins.yaml
+#EOF
+#  }
+  tags = {
+    Name = "bastion_tf"
+  }
+#  depends_on = [aws_main_route_table_association.set-master-default-rt-assoc]
 }
--- network_setup.tf
+++ network_setup.tf
@@ -43,3 +43,86 @@ resource "aws_subnet" "subnet_db" {
   availability_zone = element(data.aws_availability_zones.azs.names, 1)
   cidr_block        = "10.0.2.0/24"
 }
+
+
+#Create subnet #3  in us-east-1
+resource "aws_subnet" "subnet_bastion" {
+  provider          = aws.region-lab
+  vpc_id            = aws_vpc.vpc_useast.id
+  availability_zone = element(data.aws_availability_zones.azs.names, 2)
+  cidr_block        = "10.0.3.0/24"
+}
+
+
+resource "aws_security_group" "lb-sg" {
+  provider    = aws.region-lab
+  name        = "lb-sg"
+  description = "Allow 80 and traffic to Apache SG"
+  vpc_id      = aws_vpc.vpc_useast.id
+
+  ingress {
+    description = "Allow 80 from anywhere for redirection"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.external_ip]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.external_ip]
+  }
+}
+
+#Create SG for allowing TCP/80 from * and TCP/22 from your IP in us-east-1
+resource "aws_security_group" "app-sg" {
+  provider    = aws.region-lab
+  name        = "app-sg"
+  description = "Allow TCP/80 & TCP/22"
+  vpc_id      = aws_vpc.vpc_useast.id
+  ingress {
+    description = "Allow 22 from our public IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.subnet_bastion]
+  }
+  ingress {
+    description     = "allow traffic from LB on port 80"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb-sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.external_ip]
+  }
+}
+
+
+#Create SG for allowing TCP/80 from * and TCP/22 from your IP in us-east-1
+resource "aws_security_group" "bastion-sg" {
+  provider    = aws.region-lab
+  name        = "app-sg"
+  description = "Allow TCP/80 & TCP/22"
+  vpc_id      = aws_vpc.vpc_useast.id
+  ingress {
+    description = "Allow 22 from our public IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.external_ip]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
